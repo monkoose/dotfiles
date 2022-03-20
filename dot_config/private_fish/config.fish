5debb58fab2c43e578ebe818b61f588df339f688
monkoose <monkoose@protonmail.com>
2022-03-20 19:33:29 +0200

    Update

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+    # Start X at login
+    if status --is-login
+        if test -z "$DISPLAY" -a $XDG_VTNR = 1
+            exec startx -- -keeptty
+        end
+    end
+    # Commands to run in interactive sessions can go here
+end
+fish_add_path "$HOME/.cargo/bin"
