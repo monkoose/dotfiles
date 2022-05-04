b8a8201ba0670e034d5f634888953ac9824df041
monkoose <monkoose@protonmail.com>
2022-05-04 10:23:16 +0300

    Update

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,7 +2,6 @@ if status is-interactive
     # Start X at login
     if status --is-login
         if test -z "$DISPLAY" -a $XDG_VTNR = 1
-            # exec startx -- -keeptty
             exec sway
         end
     end
