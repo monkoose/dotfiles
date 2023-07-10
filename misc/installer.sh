f398c4a046bf85a6cf9687db68e2352155847e82
monkoose <pythonproof@gmail.com>
2023-07-10 11:02:00 +0300

    Update

--- misc/installer.sh
+++ misc/installer.sh
@@ -1,42 +1,69 @@
 #!/usr/bin/env bash
+set -eu -o pipefail
+IFS=$'\n\t'
+
+scriptpath=$(dirname $(realpath "$0"))
+cd "$scriptpath"
 
 install_if_needed() {
     yay -Qi "$1" &>/dev/null || yay -S "$1"
 }
 
-scriptpath=$(dirname $(realpath "$0"))
-cd "$scriptpath"
+_amdgpu() {
+    # Remove tearing for amdgpu driver
+    sudo mkdir -p /etc/X11/xorg.conf.d
+    sudo cp xorg/20-amdgpu.conf $_
 
-# Remove tearing for amdgpu driver
-mkdir -p /etc/X11/xorg.conf.d
-cp xorg/20-amdgpu.conf $_
+    # AMD Gpu fan speed by temperature
+    install_if_needed amdfand-bin
+    sudo systemctl enable --now amdfand.service
+}
 
-# AMD Gpu fan speed by temperature
-install_if_needed amdfand-bin
-systemctl enable --now amdfand.service
-
-# keyd
-install_if_needed keyd
-mkdir -p /etc/keyd
-cp keyd/all.conf $_
+_keyd() {
+    install_if_needed keyd
+    sudo mkdir -p /etc/keyd
+    sudo cp keyd/all.conf $_
+}
 
 # Install vim with custom features
-git clone https://github.com/vim/vim.git vim-repo
-cd vim-repo
-git apply ../vim/git-patch.patch
-make reconfig && make install
-cd "$scriptpath" && rm -rf vim-repo
+_vim() {
+    git clone --depth=1 https://github.com/vim/vim.git vim-repo
+    cd vim-repo
+    git apply ../vim/git-patch.patch
+    make reconfig && sudo make install
+    cd "$scriptpath" && rm -rf vim-repo
+}
 
 ## Update `bat` cache so it can use custom theme
-install_if_needed bat
-bat cache --build
+_bat() {
+    install_if_needed bat
+    bat cache --build
+}
 
 ## xdg-user-dirs
-install_if_needed xdg-user-dirs
-xdg-user-dirs-update --set TEMPLATES $HOME/.local/share/Templates
-xdg-user-dirs-update --set PUBLICSHARE $HOME/.local/share/Public
-xdg-user-dirs-update --set DESKTOP $HOME/.local/share/Desktop
-xdg-user-dirs-update --set DOWNLOAD $HOME/Downloads
-xdg-user-dirs-update --set DOCUMENTS $HOME/Documents
-xdg-user-dirs-update --set VIDEOS $HOME/Videos
-xdg-user-dirs-update --set PICTURES $HOME/Pictures
+_userdirs() {
+    install_if_needed xdg-user-dirs
+    xdg-user-dirs-update --set DOCUMENTS $HOME/Documents
+    xdg-user-dirs-update --set DOWNLOAD $HOME/Downloads
+    xdg-user-dirs-update --set PICTURES $HOME/Pictures
+    xdg-user-dirs-update --set VIDEOS $HOME/Videos
+    xdg-user-dirs-update --set DESKTOP $HOME/.local/share/Desktop
+    xdg-user-dirs-update --set MUSIC $HOME/.local/share/Music
+    xdg-user-dirs-update --set PUBLICSHARE $HOME/.local/share/Public
+    xdg-user-dirs-update --set TEMPLATES $HOME/.local/share/Templates
+}
+
+# main script
+config_funcs=(
+    _amdgpu
+    _keyd
+    _bat
+    _userdirs
+)
+
+for func in "${config_funcs[@]}"; do
+    echo "Running $func"
+    $func
+done
+
+echo "Done."
