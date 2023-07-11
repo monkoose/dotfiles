eb4fbd3942a885a1b8b6ee355f58bfd525f37158
monkoose <pythonproof@gmail.com>
2023-07-11 21:14:10 +0300

    Update

---
+++ dot_config/private_fish/functions/chezinstall.fish
@@ -0,0 +1,4 @@
+function chezinstall --wraps=/home/monkoose/.local/share/chezmoi/misc/installer.sh --description 'alias chezinstall /home/monkoose/.local/share/chezmoi/misc/installer.sh'
+  /home/monkoose/.local/share/chezmoi/misc/installer.sh $argv
+        
+end
--- dot_config/private_fish/functions/yar.fish
+++ dot_config/private_fish/functions/yar.fish
@@ -1,4 +1,3 @@
 function yar --wraps='yay -Rcsn' --description 'alias yar yay -Rcsn'
   yay -Rcsn $argv
-        
 end
