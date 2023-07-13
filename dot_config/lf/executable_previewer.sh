015313b2a63492f7fea4c720897ed7997392005f
monkoose <pythonproof@gmail.com>
2023-07-13 15:38:56 +0300

    Update

--- dot_config/lf/executable_previewer.sh
+++ dot_config/lf/executable_previewer.sh
@@ -1,3 +1,10 @@
-#!/bin/sh
+#!/usr/bin/env bash
+set -eu -o pipefail
+IFS=$'\n\t'
 
-bat --plain --color=always --paging=never --theme=boa -r 1:$3 "$1"
+if [[ $(xdg-mime query filetype "$1") == *zip ]]
+then
+    atool --list "$1" 2>/dev/null
+else
+    bat --plain --color=always --paging=never --theme=boa -r 1:"$3" "$1"
+fi
