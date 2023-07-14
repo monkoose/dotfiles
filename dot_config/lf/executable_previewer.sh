a6ccce6595797b1de9f165fa6b7af66a17a6379c
monkoose <pythonproof@gmail.com>
2023-07-14 22:21:04 +0300

    Add lf atool

--- dot_config/lf/executable_previewer.sh
+++ dot_config/lf/executable_previewer.sh
@@ -2,9 +2,14 @@
 set -eu -o pipefail
 IFS=$'\n\t'
 
-if [[ $(xdg-mime query filetype "$1") == *zip ]]
-then
-    atool --list "$1" 2>/dev/null
-else
-    bat --plain --color=always --paging=never --theme=boa -r 1:"$3" "$1"
-fi
+case "$(file -b --mime-type "$1")" in
+    *x-tar|*zip|*x-bzip2|*x-rar|*x-7z*)
+        atool --list "$1" 2>/dev/null
+        ;;
+    *mp4)
+        mediainfo "$1" 2>/dev/null
+        ;;
+    *)
+        bat --plain --color=always --paging=never --theme=boa -r 1:"$3" "$1"
+        ;;
+esac
